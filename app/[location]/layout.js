dd65487c19e7bfa641359aac000624626816dd62
naeemmahmud70 <naeemmahmud370@gamil.com>
2025-05-20 23:34:49 +0600

    aqi, temperature, weather, and wind page addeed through parralal routing and integrated apis

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import "../globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+  weather,
+  aqi,
+  wind,
+  temperature,
+}) {
   return (
     <div className="wrapper">
       <div className="overlay"></div>
@@ -16,6 +22,10 @@ export default function RootLayout({ children }) {
         <div className="container">
           <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
             {children}
+            {weather}
+            {aqi}
+            {wind}
+            {temperature}
           </div>
         </div>
       </main>
